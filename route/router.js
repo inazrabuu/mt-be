232490e1637b6c5a44c9cbf9fbc7850ba4712bbb
Arman Rizal <inazrabuu@gmail.com>
2021-06-01 21:34:21 +0700

    added new route

--- route/router.js
+++ route/router.js
@@ -6,5 +6,6 @@ const index = new Index()
 router.get('/', index.index)
 router.post('/get_snap', index.getSnap)
 router.post('/post_payment_handler', index.postPaymentHandler)
+router.post('/save_card', index.saveCard)
 
 module.exports = router
